a91abb78775140f51bd5b3d69f10226bb3eb5e5a
Justin Maat <justin.maat@wendys.com>
2016-03-28 12:21:19 -0400

    currying the persisState to allow for dynamic storage from client

--- src/persistState.js
+++ src/persistState.js
@@ -1,60 +1,64 @@
 import mapValues from 'lodash/mapValues';
 import identity from 'lodash/identity';
 
-export default function persistState(sessionId, deserializeState = identity, deserializeAction = identity) {
-    if (!sessionId) {
-        return next => (...args) => next(...args);
-    }
 
-    function deserialize(state) {
-        return {
-            ...state,
-            actionsById: mapValues(state.actionsById, liftedAction => ({
-                ...liftedAction,
-                action: deserializeAction(liftedAction.action)
-            })),
-            committedState: deserializeState(state.committedState),
-            computedStates: state.computedStates.map(computedState => ({
-                ...computedState,
-                state: deserializeState(computedState.state)
-            }))
-        };
-    }
+export default function persistState(storage) {
+    return (sessionId, deserializeState = identity, deserializeAction = identity) => {
 
-    return next => (reducer, initialState, enhancer) => {
-        const key = `redux-dev-session-${sessionId}`;
-
-        let finalInitialState;
-        try {
-            const json = localStorage.getItem(key);
-            if (json) {
-                finalInitialState = deserialize(JSON.parse(json)) || initialState;
-                next(reducer, initialState);
-            }
-        } catch (e) {
-            console.warn('Could not read debug session from localStorage:', e);
-            try {
-                localStorage.removeItem(key);
-            } finally {
-                finalInitialState = undefined;
-            }
+        if (!sessionId) {
+            return next => (...args) => next(...args);
         }
 
-        const store = next(reducer, finalInitialState, enhancer);
+        function deserialize(state) {
+            return {
+                ...state,
+                actionsById: mapValues(state.actionsById, liftedAction => ({
+                    ...liftedAction,
+                    action: deserializeAction(liftedAction.action)
+                })),
+                committedState: deserializeState(state.committedState),
+                computedStates: state.computedStates.map(computedState => ({
+                    ...computedState,
+                    state: deserializeState(computedState.state)
+                }))
+            };
+        }
 
-        return {
-            ...store,
-            dispatch(action) {
-                store.dispatch(action);
+        return next => (reducer, initialState, enhancer) => {
+            const key = `redux-dev-session-${sessionId}`;
 
-                try {
-                    localStorage.setItem(key, JSON.stringify(store.getState()));
-                } catch (e) {
-                    console.warn('Could not write debug session to localStorage:', e);
+            let finalInitialState;
+            try {
+                const json = storage.getItem(key);
+                if (json) {
+                    finalInitialState = deserialize(JSON.parse(json)) || initialState;
+                    next(reducer, initialState);
                 }
-
-                return action;
+            } catch (e) {
+                console.warn('Could not read debug session from localStorage:', e);
+                try {
+                    storage.removeItem(key);
+                } finally {
+                    finalInitialState = undefined;
+                }
             }
+
+            const store = next(reducer, finalInitialState, enhancer);
+
+            return {
+                ...store,
+                dispatch(action) {
+                    store.dispatch(action);
+
+                    try {
+                        storage.setItem(key, JSON.stringify(store.getState()));
+                    } catch (e) {
+                        console.warn('Could not write debug session to localStorage:', e);
+                    }
+
+                    return action;
+                }
+            };
         };
-    };
+    }
 }
